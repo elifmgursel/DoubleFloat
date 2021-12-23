a7446143b8ceec4034ff56158cb24206c2064255
Elif Mürvet Gürsel <gurselelif@gmail.com>
2021-12-23 13:10:52 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Merhaba Java");
+
+        int elf = 6 ;
+        int b = 8;
+        b = 8 ;
+        int c = 5;
+
+        int toplam = elf+b+c;
+
+        System.out.println("Toplam = " + toplam);
+
+
+    }
+}
